eb48c4f533b092973b419ab2efd0a4ee6aefc26f
Will Funnell <w.f.funnell@gmail.com>
2014-03-31 18:53:42 +0100

    changed to kittens webserver

--- src/ex_web/webserver.clj
+++ src/ex_web/webserver.clj
@@ -10,7 +10,7 @@
             [ex-web.database :as db]))
 
 (defn index-page
-  []
+  [kitten-part]
   (html5 {:lang "en"}
          [:head
           [:title "What's New Pussycat?"]
@@ -21,22 +21,34 @@
            [:div.navbar-header
             [:div.navbar-brand "What's New Pussycat?"]]]
           [:div#main.container
-           [:img.thumbnail {:src "/kitty.jpg"
-                            :alt "Kitty"}]
+           kitten-part
            [:div "The time is "
             [:span.label.label-default (db/current-time)]]]]))
 
-(defroutes main-routes
-  (GET "/" [] (index-page))
+(defn create-kittens [n]
+  (loop [cnt n acc []]
+    (if (zero? cnt)
+      acc
+      (recur (dec cnt) (cons [:img.thumbnail {:src (str "http://placekitten.com/200/3" (+ 10 cnt))
+                                              :alt "Kitty"}] acc)))))
 
-  (GET "/kittens/:number"
-       [number]
-       (format "Your number is: %s" number))
+(defroutes main-routes
+  (GET "/" [] (index-page (create-kittens 1)))
+
+  (GET "/kittens/:number" [] (index-page (create-kittens 10)))
 
   (route/resources "/")
   (route/not-found "Page not found"))
 
+(defn log-time [filter-chain-fn]
+ (fn [request]
+   (println (format "URI is: %s" (:uri request)))
+   (let [response (filter-chain-fn request)]
+     (println (format "Time is: %s" (. System (nanoTime))))
+     response)))
+
 (def app
   (-> #'main-routes
       handler/site
-      wrap-base-url))
+      wrap-base-url
+      (log-time)))
